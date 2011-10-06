973354575bae867be38b7e9bb775f084c31e8204
Timo Westkämper <timo.westkamper@mysema.com>
2011-10-06 21:35:01 +0300

    improved panel

--- src/panel.h
+++ src/panel.h
@@ -6,10 +6,11 @@
 
 using namespace Gtk;
 
-class Panel : public VBox {
+class Panel : public Frame {
   public:
     Panel(const char* title, Widget* toggle, Widget* content) {
         set_border_width(5);
+        set_shadow_type(SHADOW_OUT);
         strcpy(bold_title, "<b>");
         strcat(bold_title, title);
         strcat(bold_title, "</b>");
@@ -17,11 +18,14 @@ class Panel : public VBox {
         Label* label = manage(new Label(bold_title));
         label->set_use_markup();   
 
+        VBox* main = manage(new VBox());
         HBox* header = manage(new HBox());
         header->pack_start(*label, false, false);
         header->pack_end(*toggle, false, false);
-        pack_start(*header);
-        pack_start(*content);
+        main->set_border_width(2);
+        main->pack_start(*header);
+        main->pack_start(*content);
+        add(*main);
     }
 
   private:
